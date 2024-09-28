eb53cafa0b55dc1b1f9c56f621014fba84171fc7
mohsuwan <suwanabdnabeelmoh@gmail.com>
2024-09-28 11:45:08 +0300

    change the sub category componant from

---
+++ src/pages/subcategory/SubCategory.tsx
@@ -0,0 +1,68 @@
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+import * as React from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  // SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface SubCategory {
+  id: number;
+  name: string;
+}
+function SubCategory({
+  subCategoryData,
+}: // callBack,
+// clear,
+{
+  subCategoryData: SubCategory[];
+  callBack: any;
+}) {
+  console.log(subCategoryData);
+
+  return (
+    <Select onOpenChange={(event) => event}>
+      <SelectTrigger dir="rtl" className="w-full cursor-pointer">
+        <SelectValue placeholder="اختر الفرع" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {/* <SelectLabel value={""}>فرع</SelectLabel> */}
+          {subCategoryData?.map(({ name, id }, index) => {
+            return (
+              <>
+                <SelectItem key={index} value={id}>
+                  {name}
+                </SelectItem>
+              </>
+            );
+          })}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+
+  // return (
+  //   <div>
+  //     <select onChange={(event) => callBack(event.target.value)}>
+  //       <option value={""}>اختر الفرع</option>
+  //       {subCategoryData?.map(({ name, id }, index) => {
+  //         return (
+  //           <>
+  //             <option key={index} value={id}>
+  //               {name}
+  //             </option>
+  //           </>
+  //         );
+  //       })}
+  //     </select>
+  //   </div>
+  // );
+}
+export default SubCategory;
