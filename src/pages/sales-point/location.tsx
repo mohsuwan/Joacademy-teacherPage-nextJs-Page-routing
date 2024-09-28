fe1950d0396a723c624441ba13add3961b99ec26
mohsuwan <suwanabdnabeelmoh@gmail.com>
2024-09-28 11:19:18 +0300

    delete file react dont used

--- src/pages/sales-point/location.tsx
+++
@@ -1,7 +0,0 @@
-// import dynamic from "next/dynamic";
-
-// const Location = dynamic(
-//   () => import("../../components/api/sales-point/geolocation"),
-//   { ssr: false }
-// );
-// export default Location;
